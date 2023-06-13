9d3c66ceb1cb514bd3d8bba6e3f632f94754c7cc
nanekja <raja.nannapaneni@gmail.com>
2023-06-13 23:48:39 +0530

    m1 c7

--- ERA7/trails/utils1.py
+++ ERA7/trails/utils1.py
@@ -73,14 +73,14 @@ def GetCorrectPredCount(pPrediction, pLabels):
   return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
 
 def draw_graphs(train_losses, train_acc, test_losses, test_acc):
-    fig, axs = plt.subplots(2,2,figsize=(15,10))
-    axs[0, 0].plot(train_losses)
-    axs[0, 0].set_title("Training Loss")
-    axs[1, 0].plot(train_acc)
-    axs[1, 0].set_title("Training Accuracy")
-    axs[0, 1].plot(test_losses)
-    axs[0, 1].set_title("Test Loss")
-    axs[1, 1].plot(test_acc)
-    axs[1, 1].set_title("Test Accuracy")
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(t)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
 
 
