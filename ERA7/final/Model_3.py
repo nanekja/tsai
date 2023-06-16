fce3e13277c3d4c1068c85cb70b5de746d147cdc
nanekja <raja.nannapaneni@gmail.com>
2023-06-16 23:03:53 +0530

    m3 c15

--- ERA7/final/Model_3.py
+++ ERA7/final/Model_3.py
@@ -25,13 +25,13 @@ class Net(nn.Module):
             nn.ReLU()
         ) # output_size = 24
 
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 12
+
         self.convblock3 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(1, 1), padding=0, bias=False),
             nn.BatchNorm2d(8),
             nn.ReLU()
-        ) # output_size = 24
-
-        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 12
+        ) # output_size = 12
 
         # CONVOLUTION BLOCK 2
 
@@ -43,42 +43,42 @@ class Net(nn.Module):
 
         # OUTPUT BLOCK
         self.convblock5 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(3, 3), padding=0, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.BatchNorm2d(8),
             nn.ReLU()
         ) # output_size = 8
         self.convblock6 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
-            nn.BatchNorm2d(16),
-            nn.ReLU()
-        ) # output_size = 6
-        self.convblock7 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
-            nn.BatchNorm2d(16),
-            nn.ReLU()
+#            nn.BatchNorm2d(16),
+#            nn.ReLU()
+        ) # output_size = 6
+
+        self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=6)) # output_size = 1
+
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+
         ) # output_size = 4
-        self.pool2 = nn.MaxPool2d(2, 2) # output_size = 3
-         # output_size = 2
-        self.gap = nn.Sequential(
-            nn.AvgPool2d(kernel_size=2)
-        ) # output_size = 1
+
         self.dropout = nn.Dropout(0.1)
    
 
     def forward(self, x):
         x = self.convblock1(x)
         x = self.convblock2(x)
+        x = self.pool1(x)
         x = self.dropout(x)
         x = self.convblock3(x)
-        x = self.pool1(x)
+
         x = self.convblock4(x)
         x = self.dropout(x)
         x = self.convblock5(x)
         x = self.dropout(x)
         x = self.convblock6(x)
         x = self.dropout(x)
-        x = self.convblock7(x)
-        x = self.pool2(x)        
         x = self.gap(x)
+        x = self.convblock7(x)
+      
+
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
