cf05266f8c7259fb78112d3a80b1a7863633d718
nanekja <raja.nannapaneni@gmail.com>
2023-06-29 19:34:16 +0530

    m1 c2

--- ERA9/trails/model1.py
+++ ERA9/trails/model1.py
@@ -6,208 +6,102 @@ from torchsummary import summary
 from tqdm import tqdm
 from utils import *
 
-GROUP_SIZE = 2
-
+dropout_value = 0.1
 class Net(nn.Module):
-    def __init__(self, norm):
+    def __init__(self):
         super(Net, self).__init__()
-
-
-        # Input Block
+        # CONVOLUTION BLOCK 1 input 32/1/1
         self.convblock1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(7, 7), padding=3, bias=False),
             nn.ReLU(),
-        ) # output_size = 32
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 32/3
 
-        out_channels=32
-        if norm == 'bn':
-            self.n1 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n1 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n1 = nn.GroupNorm(1, out_channels)
-
+        
         self.convblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
-        ) # output_size = 32
-
-        out_channels = 64
-        if norm == 'bn':
-            self.n2 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n2 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n2 = nn.GroupNorm(1, out_channels)
-
-
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 32/5
 
         # TRANSITION BLOCK 1
         self.convblock3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=16, kernel_size=(1, 1), padding=0, bias=False),
-            nn.ReLU(),
-        ) # output_size = 32
-
-        out_channels = 16
-        if norm == 'bn':
-            self.n3 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n3 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n3 = nn.GroupNorm(1, out_channels)
-
-        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 16
-
-
+            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(1, 1), padding=0, bias=False),
+        ) # output_size = 32/5
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 16/6/2
 
         # CONVOLUTION BLOCK 2
         self.convblock4 = nn.Sequential(
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
-            nn.ReLU(),
-        ) # output_size = 16
-
-        out_channels=32
-        if norm == 'bn':
-            self.n4 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n4 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n4 = nn.GroupNorm(1, out_channels)
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 16/10/2
 
         self.convblock5 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
-            nn.ReLU(),
-        ) # output_size = 16
-
-        out_channels=32
-        if norm == 'bn':
-            self.n5 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n5 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n5 = nn.GroupNorm(1, out_channels)
-
-        self.convblock6 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
-            nn.ReLU(),
-        ) # output_size = 14
-
-        out_channels=32
-        if norm == 'bn':
-            self.n6 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n6 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n6 = nn.GroupNorm(1, out_channels)
-
-
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 16/14/2
 
         # TRANSITION BLOCK 2
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(1, 1), padding=0, bias=False),
+        ) # output_size = 16/14/2
+        self.pool2 = nn.MaxPool2d(2, 2) # output_size = 8/16/4
+
         self.convblock7 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
-            nn.ReLU(),
-        ) # output_size = 14
-
-        out_channels=10
-        if norm == 'bn':
-            self.n7 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n7 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n7 = nn.GroupNorm(1, out_channels)
-
-        self.pool2 = nn.MaxPool2d(2, 2) # output_size = 7
-
-
-
-       # CONVOLUTION BLOCK 3
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 8/24/4
         self.convblock8 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=1, bias=False),
-            nn.ReLU(),
-        ) # output_size = 7
-
-        out_channels=10
-        if norm == 'bn':
-            self.n8 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n8 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n8 = nn.GroupNorm(1, out_channels)
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 6/32/4
 
         self.convblock9 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
-            nn.ReLU(),
-        ) # output_size = 5
-
-        out_channels=10
-        if norm == 'bn':
-            self.n9 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n9 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n9 = nn.GroupNorm(1, out_channels)
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),            
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value)
+        ) # output_size = 4/40/4
+        
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=4)
+        ) # output_size = 1
 
         self.convblock10 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
-            nn.ReLU(),
-        ) # output_size = 3
-
-        out_channels=10
-        if norm == 'bn':
-            self.n10 = nn.BatchNorm2d(out_channels)
-        elif norm == 'gn':
-            self.n10 = nn.GroupNorm(GROUP_SIZE, out_channels)
-        elif norm == 'ln':
-            self.n10 = nn.GroupNorm(1, out_channels)
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            # nn.BatchNorm2d(10),
+            # nn.ReLU(),
+            # nn.Dropout(dropout_value)
+        ) 
 
 
-
-        # OUTPUT BLOCK
-
-        self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=3)) # output_size = 1
-
-        self.convblock11 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)
-        )
-        self.dropout = nn.Dropout(0.1)
-   
+        self.dropout = nn.Dropout(dropout_value)
 
     def forward(self, x):
         x = self.convblock1(x)
-        x = self.n1(x)
-        x = self.dropout(x)
-        x = self.convblock2(x)
-        x = self.n2(x)
-        x = self.dropout(x)
-
+        x = x + self.convblock2(x)
         x = self.convblock3(x)
-        x = self.n3(x)
         x = self.pool1(x)
-
         x = self.convblock4(x)
-        x = self.n4(x)
-        x = self.dropout(x)
-        x = self.convblock5(x)
-        x = self.n5(x)
-        x = self.dropout(x)
+        x = x + self.convblock5(x)
         x = self.convblock6(x)
-        x = self.n6(x)
-        x = self.dropout(x)
-
-        x = self.convblock7(x)
-        x = self.n7(x)
         x = self.pool1(x)
-
+        x = self.convblock7(x)
         x = self.convblock8(x)
-        x = self.n8(x)
-        x = self.dropout(x)
         x = self.convblock9(x)
-        x = self.n9(x)
-        x = self.dropout(x)
+        x = self.gap(x)        
         x = self.convblock10(x)
-        x = self.n10(x)
 
-        x = self.gap(x)
-        #x = self.convblock11(x)
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
